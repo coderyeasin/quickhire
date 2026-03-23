e70c4ac2449f72ed6594984c29638fc43fe2f646
coderyeasin <yeasin3233@gmail.com>
2026-03-24 00:05:19 +0600

    provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,8 @@
+import { loginValidation } from "@/components/Auth/Login/LoginValidators";
+import { UserModel } from "@/modules/user/user.model";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { email } from "zod";
 
 export const { signIn, signOut, auth, handlers } = NextAuth({
   providers: [
@@ -8,7 +11,17 @@ export const { signIn, signOut, auth, handlers } = NextAuth({
         email: {},
         password: {},
       },
-      authorize: async (credentials) => {},
+      authorize: async (credentials) => {
+        let user = null;
+        const validatorsLogin = loginValidation.parse(credentials);
+        if (validatorsLogin.email) {
+          user = await UserModel.findOne({
+            where: {
+              email: validatorsLogin.email,
+            },
+          });
+        }
+      },
     }),
   ],
   pages: {
