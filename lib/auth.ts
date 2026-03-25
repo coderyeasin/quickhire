c904824702dcb4f91fd89f92a01249c55a7cb25e
coderyeasin <yeasin3233@gmail.com>
2026-03-25 16:45:11 +0600

    provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,6 @@ import { loginValidation } from "@/components/Auth/Login/LoginValidators";
 import { UserModel } from "@/modules/user/user.model";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { email } from "zod";
 
 export const { signIn, signOut, auth, handlers } = NextAuth({
   providers: [
