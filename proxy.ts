0906015c5a04e12a2dc2e83447258da0f857757a
coderyeasin <yeasin3233@gmail.com>
2026-03-31 15:55:09 +0600

    auth

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,12 @@
 import { auth } from "@/lib/auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function proxy(req: NextRequest) {
+export async function proxy(
+  req: NextRequest,
+  requiredRole: "admin" | "candidate",
+) {
   const session = await auth();
-  if (!session || session.user.role) {
+  if (!session || session.user.role !== requiredRole) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
   return NextResponse.next();
