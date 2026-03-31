dd9cdf62e94d6a879a5319fa8cc830591c77caa7
coderyeasin <yeasin3233@gmail.com>
2026-03-31 15:58:40 +0600

    auth

--- components/Auth/Login/Login.tsx
+++ components/Auth/Login/Login.tsx
@@ -4,10 +4,9 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FaFacebook, FaGoogle } from "react-icons/fa6";
-import Link from "next/link";
 import { useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { signIn, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { loginValidation } from "./LoginValidators";
 
 type LoginFormData = z.infer<typeof loginValidation>;
