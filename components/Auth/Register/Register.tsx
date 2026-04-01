1278d8715b1f159b57dd70502d1dc5edcec9a8a5
coderyeasin <yeasin3233@gmail.com>
2026-04-01 15:23:21 +0600

    auth

--- components/Auth/Register/Register.tsx
+++ components/Auth/Register/Register.tsx
@@ -19,6 +19,7 @@ const RegisterPage = ({ onSuccess }) => {
   });
   const [serverError, setServerError] = useState<string | null>(null);
   const [success, setSuccess] = useState(false);
+  // const router = useRouter();
 
   const onSubmit = async (data: RegisterFormData) => {
     setServerError(null);
