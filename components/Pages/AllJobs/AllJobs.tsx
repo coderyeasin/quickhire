65f94dddc6c88e41e113dbddc8881dc561c0e202
coderyeasin <yeasin3233@gmail.com>
2026-04-20 12:07:20 +0600

    polish

--- components/Pages/AllJobs/AllJobs.tsx
+++ components/Pages/AllJobs/AllJobs.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const AllJobs = () => {
   return (
     <section className="container-layout h-screen w-full py-10">
-      <div>AllJobs</div>
+      <div>AllJobs --- will be listed here</div>
     </section>
   );
 };
