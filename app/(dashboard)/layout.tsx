c6a43f96a27bcdf111f29015df0d5c8607867152
coderyeasin <yeasin3233@gmail.com>
2026-04-28 12:44:44 +0600

    be-done

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -10,12 +10,6 @@ export default async function DashboardLayout({
 }) {
   const session = await auth();
   if (!session?.user) redirect("/");
-  // role={session.user.role}
-
-  // global error handling
-  // not found - route handling
-  // responsiveness ensure
-  // catchAsync - function create to reduce
 
   return (
     <div className="flex min-h-screen bg-[#F8F8FD]">
