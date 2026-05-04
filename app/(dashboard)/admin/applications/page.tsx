620ec3abdaac867bbbfefd74761c0d30d5e51d3a
coderyeasin <yeasin3233@gmail.com>
2026-05-04 23:24:11 +0600

    applications

---
+++ app/(dashboard)/admin/applications/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminApplicationsPage = () => {
+  return (
+    <div>
+      <h3>AdminApplicationsPage</h3>
+    </div>
+  );
+};
+
+export default AdminApplicationsPage;
