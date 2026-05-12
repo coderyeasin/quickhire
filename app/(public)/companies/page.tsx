90f2bb6f42336b9a7c8bf572c043f2442126ebe9
coderyeasin <yeasin3233@gmail.com>
2026-05-12 20:11:33 +0600

    fix-issues

--- app/(public)/companies/page.tsx
+++ app/(public)/companies/page.tsx
@@ -2,7 +2,7 @@ import CompaniesJobs from "@/components/Pages/CompaniesJobs/CompaniesJobs";
 
 const CompaniesHome = () => {
   return (
-    <section className="container-layout h-screen">
+    <section className="container-layout h-auto py-10">
       <CompaniesJobs />
     </section>
   );
